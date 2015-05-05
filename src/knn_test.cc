44acdf3fd8606e8862aeb4779d642abc854a7d0f
Sharon Yang <sharonyang@caltech.edu>
2015-05-05 12:49:23 -0700

    KNN refactor completed. Tested and runs normally.
    Maybe more optimization? This gives the same RMSE as before
    the refactor.

--- src/knn_test.cc
+++ src/knn_test.cc
@@ -16,33 +16,203 @@
 #include <fstream>
 #include <stdexcept>
 
-#include "knn.hh"
-#include "netflix.hh"
+#include <netflix.hh>
+#include <knn.hh>
 
 using namespace std;
 using namespace arma;
 using namespace netflix; // challenge-related constants/functions.
 
-// The file path we use to train, to test, and to output.
-const string TRAIN_PATH_UM =    "../../netflix_challenge/data/train.dta";
-const string TRAIN_PATH_MU =    "../../netflix_challenge/data/train-mu.dta";
-const string P_PATH =           "../../netflix_challenge/data/knn-p.dta";
-const string QUAL_PATH =        "../../netflix_challenge/data/um/new_qual.dta";
-const string OUTPUT_PATH =      "data/output_knn.dta";
-const bool SAVE_P = false;
+
+// The Armadillo binary file to use for training.
+// Make sure that both UM and MU data are using the same
+// "type" of training resource (eg. probe, hidden, base, etc.)
+const string TRAIN_UM = BASE_BIN;
+
+// Minimum common neighbors required for decent prediction.
+const int MIN_COMMON = 16;
+
+// Max weight elements to consider when predicting.
+const unsigned int MAX_WEIGHT = 10;
+
+// If P is already precomputed, only need to load.
+const bool LOAD_P = false;
+
+// Whether we want to save the output of P.
+const bool SAVE_P = true;
+
+// Sig-figs for output file.
+const int RATING_SIG_FIGS = 4;
+
+// If we do want to load or save, the location of P file.
+const string P_PATH = "data/knn-p.dta";
+
+// The name of the output file to use (for predictions on "qual").
+const string OUTPUT_FN = "data/knn_VALID_predictions.dta";
+
+// Helper function that carries out "predAlgo" on the test file specified
+// by testFileName, and then puts the prediction results (for each (user,
+// item, time) in testFileName) in outputFileName.
+void testOnDataFile(KNN *predAlgo, const string &testFileName,
+                    const string &outputFileName);
+
+// Helper function that carries out "predAlgo" on the test file specified
+// by testFileName (most likely the "probe" dataset), and calculates the
+// RMSE based on those results. Note that testFileName must refer to an
+// Armadillo binary in this case.
+float computeRMSE(KNN *predAlgo, const string &testFileName);
 
 int main(void)
 {
+    cout << "Start KNN..." << endl;
+    cout << "Load UM matrix..." << endl;
+    // Load from binary.
+    fmat trainingSetUM;
+    trainingSetUM.load(TRAIN_UM, arma_binary);
+    cout << "Finished loading UM matrix." << endl;
+
     // Initializing the KNN.
-    KNN *knn = new KNN(TRAIN_PATH_UM, TRAIN_PATH_MU, P_PATH,
-    	QUAL_PATH, OUTPUT_PATH);
-    knn->loadData();
-    if (SAVE_P)
+    //KNN knn(NUM_USERS, NUM_MOVIES, MIN_COMMON, MAX_WEIGHT,
+        //TRAIN_MU, P_PATH);
+    KNN *knn = new KNN(NUM_USERS, NUM_MOVIES, MIN_COMMON, MAX_WEIGHT,
+        P_PATH);
+    knn->train(trainingSetUM);
+    if (LOAD_P)
+        knn->loadP();
+    else
     {
-    	knn->calcP();
-    	knn->saveP();
+        knn->calcP();
+        if (SAVE_P)
+            knn->saveP();
     }
-    knn->loadP();
-    knn->output();
+
+    // Go through qual.dta to produce a prediction file.
+    testOnDataFile(knn, QUAL_DATA_FN, OUTPUT_FN);
+
+    // Get probe RMSE.
+    float probeRMSE = computeRMSE(knn, PROBE_BIN);
+
+    cout << "Probe RMSE: " << probeRMSE << endl;
+
     cout << "KNN completed.\n";
+    /*
+    knn.train(trainingSetUM);
+    if (LOAD_P)
+        knn.loadP();
+    else
+    {
+        knn.calcP();
+        if (SAVE_P)
+            knn.saveP();
+    }
+
+    // Go through qual.dta to produce a prediction file.
+    testOnDataFile(knn, QUAL_DATA_FN, OUTPUT_FN);
+
+    // Get probe RMSE.
+    float probeRMSE = computeRMSE(knn, PROBE_BIN);
+
+    cout << "Probe RMSE: " << probeRMSE << endl;
+
+    cout << "KNN completed.\n";
+    */
+}
+
+/**
+ * Compute the RMSE of a given prediction algorithm on a certain set of
+ * data. Note that testFileName must refer to an **Armadillo binary** in
+ * this case. This binary must represent a 4 x N matrix, where N is the
+ * number of test points.
+ *
+ */
+float computeRMSE(KNN *predAlgo, const string &testFileName)
+{
+    // Load from binary.
+    fmat testSet;
+    testSet.load(testFileName, arma_binary);
+
+    // The test set should have four rows.
+    if (testSet.n_rows != 4)
+    {
+        throw invalid_argument("File " + testFileName + " did not have " +
+                               "four rows!");
+    }
+
+    // The number we divide by in computing the RMSE.
+    int nMinusOne = testSet.n_cols - 1;
+
+    // Accumulator for RMSE (take square root at the end)
+    float rmse = 0.0;
+
+    for (unsigned int i = 0; i < testSet.n_cols; i ++)
+    {
+        int user = roundToInt(testSet(USER_ROW, i));
+        int item = roundToInt(testSet(MOVIE_ROW, i));
+        int date = roundToInt(testSet(DATE_ROW, i));
+        float actualRating = testSet(RATING_ROW, i);
+        
+        float prediction = predAlgo->predict(user, item, date, true);
+        
+        rmse += pow(actualRating - prediction, 2.0)/nMinusOne;
+    }
+
+    return sqrt(rmse);
+}
+
+
+/**
+ * Goes through the test file and saves the prediction results to the
+ * specified output file.
+ *
+ */
+void testOnDataFile(KNN *predAlgo, const string &testFileName,
+                    const string &outputFileName)
+{
+    ifstream testDataFile(testFileName);
+    ofstream outputFile(outputFileName); 
+    
+    if (testDataFile.fail())
+    {
+        throw runtime_error("Couldn't find test file at " + testFileName);
+    }
+
+    if (outputFile.fail())
+    {
+        throw runtime_error("Couldn't open output file at " 
+                            + outputFileName);
+    }
+
+    string testDataLine;
+    
+    cout << "\nTesting on data in " << testFileName << "..." << endl;
+
+    while (getline(testDataFile, testDataLine))
+    {
+        // Read the line and split it.
+        vector<int> thisLineVec;
+        splitIntoInts(testDataLine, DELIMITER, thisLineVec);
+
+        if (thisLineVec.size() != 3)
+        {
+            throw logic_error("The line \"" + testDataLine + "\" did not "
+                              "contain three delimiter-separated "
+                              "entries!");
+        }
+        
+        // The first entry is the user ID, the second entry is the item ID,
+        // and the third entry is the date. All of these should be
+        // zero-indexed!
+        int user = thisLineVec[0];
+        int item = thisLineVec[1];
+        int date = thisLineVec[2];
+        
+        // Output the prediction to file.
+        float prediction = predAlgo->predict(user, item, date, true);
+        outputFile << setprecision(RATING_SIG_FIGS) << prediction << endl;
+    }
+    
+    outputFile.close();
+
+    cout << "\nOutputted predictions on " << testFileName << " to the " 
+        "output file " << outputFileName << endl;
 }
